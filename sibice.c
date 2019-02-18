dc3e3476077a78db0bfc00981ecb27eba76641d5
Noah Purcell <njpurcell@coe.edu>
2019-02-17 20:59:36 -0600

    sibice solved

--- sibice.c
+++ sibice.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include <math.h>
 
 int main()
 {
-	
+	int n, w, h, matchlength;
+	scanf("%d %d %d", &n, &w, &h);
+	float hyp = sqrt(pow(w,2) + pow(h,2));
+
+	for (int i = 0; i < n; i++){
+		scanf("%d", &matchlength);
+		if (matchlength <= hyp)
+			printf("DA\n");
+		else
+			printf("NE\n");
+	}
 }
